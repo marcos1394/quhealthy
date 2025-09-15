2d6cd61395e6f47e3fcb00a495c70f2be8ebe540
marcos sandoval <marcosbancaprepa@gmail.com>
2025-09-15 14:21:05 -0600

    se configura stripe connect

--- next.config.ts
+++ next.config.ts
@@ -18,10 +18,21 @@ const nextConfig = {
 
   // === CABECERAS DE SEGURIDAD ===
   async headers() {
+    const cspHeader = `
+        default-src 'self';
+        script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.stripe.com https://*.stripecdn.com;
+        style-src 'self' 'unsafe-inline' https://*.stripe.com;
+        img-src 'self' 'unsafe-inline' data: https://*.stripe.com https://*.stripecdn.com;
+        font-src 'self' data:;
+        frame-src 'self' https://*.stripe.com;
+        connect-src 'self' https://*.stripe.com;
+    `.replace(/\s{2,}/g, ' ').trim(); // Limpia espacios extra
+
     return [
       {
         source: '/:path*',
         headers: [
+          { key: 'Content-Security-Policy', value: cspHeader },
           { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
           { key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubDomains; preload' },
           { key: 'X-Content-Type-Options', value: 'nosniff' },
