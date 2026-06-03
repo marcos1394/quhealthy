85718bb2a43df38e657d0b2986db85aa17365992
marcos sandoval <ingsandoval1394@gmail.com>
2026-06-02 19:09:08 -0700

    agregar denominaciones al momento de cobrar en efectivo

--- components/ui/select.tsx
+++ components/ui/select.tsx
@@ -44,7 +44,7 @@ const SelectContent = React.forwardRef<
       <SelectPrimitive.Content
         ref={ref}
         className={cn(
-          "relative z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md animate-in fade-in-80",
+          "relative z-50 min-w-[8rem] overflow-hidden rounded-md border bg-white dark:bg-slate-900 text-slate-900 dark:text-slate-100 shadow-md animate-in fade-in-80",
           position === "popper" ? "translate-y-1" : "",
           className
         )}
@@ -86,7 +86,7 @@ const SelectItem = React.forwardRef<
   <SelectPrimitive.Item
     ref={ref}
     className={cn(
-      "relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+      "relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-slate-100 dark:focus:bg-slate-800 focus:text-slate-900 dark:focus:text-slate-100 data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
       className
     )}
     {...props}
