91267d0c3b570d54dbbdf16dc597b80b7cbfa225
marcos sandoval <ingsandoval1394@gmail.com>
2026-06-20 08:15:57 -0700

    prueba de correccion navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link, useRouter, usePathname } from "@/i18n/routing";
 import { motion, AnimatePresence } from "framer-motion";
 import {
