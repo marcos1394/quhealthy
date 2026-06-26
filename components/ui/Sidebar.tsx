c7bbfd48edd45db788702720e8eb9414678b0ea5
marcos1394 <ingsandoval1394@gmail.com>
2026-06-26 00:38:23 -0700

    correcion en manejo de roles

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -65,7 +65,7 @@ export const Sidebar: React.FC<{ className?: string }> = ({ className = "" }) =>
     return <aside className="hidden md:flex flex-col w-64 bg-slate-900 dark:bg-slate-950 animate-pulse"></aside>;
   }
 
-  if (!user || user.role !== 'PROVIDER') {
+  if (!user || user.role !== 'ROLE_PROVIDER') {
     return null;
   }
 
