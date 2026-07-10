f6ed2180d67088116d0895be7305ffd24c9e2b29
marcos1394 <ingsandoval1394@gmail.com>
2026-07-10 13:08:18 -0700

    manejo correcto de categorias

--- app/[locale]/(platform)/provider/dashboard/biomedical/equipments/RegisterEquipmentDrawer.tsx
+++ app/[locale]/(platform)/provider/dashboard/biomedical/equipments/RegisterEquipmentDrawer.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { Save, X, Activity } from 'lucide-react';
 import { toast } from 'react-toastify';
