43ec843d33cd0261d9dc13456d58e35c2f5a7e2a
marcos1394 <ingsandoval1394@gmail.com>
2026-07-13 14:55:23 -0700

    correccion de planes

--- app/[locale]/(public)/business/page.tsx
+++ app/[locale]/(public)/business/page.tsx
@@ -338,7 +338,11 @@ export default function BusinessPage() {
  </ul>
 
  <Link
- href={isAuthenticated && role === 'ROLE_PROVIDER' ? `/provider/settings/subscription?planId=${plan.originalId}` : `/provider/register?planId=${plan.originalId}`}
+ href={
+    (plan as any).planKey === 'enterprise' 
+    ? '/contact' 
+    : (isAuthenticated && role === 'ROLE_PROVIDER' ? `/provider/settings/subscription?planId=${plan.originalId}` : `/provider/register?planId=${plan.originalId}`)
+ }
  className={`inline-flex items-center justify-center w-full py-4 rounded-none text-xs font-bold uppercase tracking-widest transition-colors group/btn ${
  plan.isPopular
  ? "bg-white text-black dark:bg-black dark:text-white hover:bg-gray-200 dark:hover:bg-gray-800"
